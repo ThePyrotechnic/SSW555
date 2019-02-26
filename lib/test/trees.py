286cde5b1244ecdce1527fcbbf7e7a85f45ac537
ThePyrotechnic <electricpyrotechnic@gmail.com>
2019-02-25 20:30:27 -0500

    added test for US26

--- lib/test/trees.py
+++ lib/test/trees.py
@@ -4,3 +4,4 @@ from lib.GedBuilder import Builder
 
 kristen_tree = Builder().build_tree(os.path.join('ged_files', 'kristen_test.ged'))
 michael_tree = Builder().build_tree(os.path.join('ged_files', 'michael_test.ged'))
+michael_dup_names = Builder().build_tree(os.path.join('ged_files', 'michael_dup_names.ged'))
