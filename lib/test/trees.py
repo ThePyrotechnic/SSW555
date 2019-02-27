96ace856f92c2bea5323267154b8331257dcf6ba
ThePyrotechnic <michaelmanis@tutanota.com>
2019-02-27 18:39:18 -0500

    Update trees.py

--- lib/test/trees.py
+++ lib/test/trees.py
@@ -2,6 +2,8 @@ import os
 
 from lib.GedBuilder import Builder
 
-kristen_tree = Builder().build_tree(os.path.join('ged_files', 'kristen_test.ged'))
-michael_tree = Builder().build_tree(os.path.join('ged_files', 'michael_test.ged'))
-michael_dup_names = Builder().build_tree(os.path.join('ged_files', 'michael_dup_names.ged'))
+GED_DIR = os.path.join('lib', 'test', 'ged_files')
+
+kristen_tree = Builder().build_tree(os.path.join(GED_DIR, 'kristen_test.ged'))
+michael_tree = Builder().build_tree(os.path.join(GED_DIR, 'michael_test.ged'))
+michael_dup_names = Builder().build_tree(os.path.join(GED_DIR, 'michael_dup_names.ged'))
