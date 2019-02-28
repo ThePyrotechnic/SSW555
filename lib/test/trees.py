606b488de5b9f8c8ef179ba0a6e063972a5f0da0
kristentan1 <ktan1@stevens.edu>
2019-02-27 20:51:00 -0500

    test dir update

--- lib/test/trees.py
+++ lib/test/trees.py
@@ -2,7 +2,7 @@ import os
 
 from lib.GedBuilder import Builder
 
-GED_DIR = os.path.join('ged_files')
+GED_DIR = os.path.join('lib', 'test', 'ged_files')
 
 kristen_tree = Builder().build_tree(os.path.join(GED_DIR, 'kristen_test.ged'))
 michael_tree = Builder().build_tree(os.path.join(GED_DIR, 'michael_test.ged'))
