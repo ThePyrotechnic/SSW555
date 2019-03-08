5dd0b024ba441ec2df09274c87b66c4a5655dcfc
nnekaou <nnekaou@gmail.com>
2019-03-08 15:55:26 -0500

    added indents lol

--- lib/test/US17_TestParentChildMarriage.py
+++ lib/test/US17_TestParentChildMarriage.py
@@ -15,16 +15,16 @@ _individuals = [
 ]
 
 _families = [
-    Family('F1', husband_id = '1', wife_id = '2', married = datetime(day = 1, month = 1 , year = 1948)),
+    Family('F1', husband_id = '1', wife_id = '2', child_id = '2', married = datetime(day = 1, month = 1 , year = 1948)),
     Family('F2', husband_id = '3', wife_id = '4', married = datetime(day = 1, month = 1 , year = 1963)),
-    Family('F3', husband_id = '5', wife_id = '6', married = datetime(day = 1, month = 1 , year = 1951)),
+    Family('F3', husband_id = '5', wife_id = '6', child_id = '5', married = datetime(day = 1, month = 1 , year = 1951)),
 ]
 
-born_wed = Tree()
+in_wed = Tree()
 
 [born_wed.add_individual(i) for i in _individuals]
 [born_wed.add_family(f) for f in _families]
 
 class Test_Birth_Before_Marriage(unittest.TestCase):
     def test_birth_before_marriage(self):
-        self.assertEqual(born_wed.birth_pre_marriage(), False, True, False)
+        self.assertEqual(in_wed.parent_not_spouse(), False, True, False)
